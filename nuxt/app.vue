6afc652df98c6f06d60b649b41ba3ea437e89486
koppacha <pikminchallenge@gmail.com>
2023-01-03 18:26:44 +0900

    Readme改修、CRUDのリアルタイム反映とConfirm追加

--- nuxt/app.vue
+++ nuxt/app.vue
@@ -2,10 +2,25 @@
   <div>
     <!-- Remove this component to get started! -->
     <!--    <NuxtWelcome />-->
+    名前<br>
     <q-input model-value="" v-model="names"/><br>
+    <br>
+    内容<br>
     <q-input model-value="" v-model="details"/><br>
-    <q-btn color="black" label="送信" @click="post" />
-    {{ names }}
+    <q-btn label="Confirm" color="primary" @click="confirm = true"></q-btn>
+    <q-dialog v-model="confirm" persistent>
+      <q-card>
+        <q-card-section class="row items-center">
+          <span class="q-ml-sm">本当に送信していいですか？</span>
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn flat label="キャンセル" color="primary" v-close-popup></q-btn>
+          <q-btn flat label="送信" color="primary" v-close-popup @click="post();refresh()"></q-btn>
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+
+    {{ data }}
   </div>
 </template>
 
@@ -14,6 +29,7 @@
  import {useFetch} from "nuxt/app";
 
  let id = 0;
+ let confirm = ref(false);
 
  const names = ref('');
  const details = ref('');
@@ -28,7 +44,6 @@
           }
         });
   }
-
-  // const { data, error } = await useFetch('/api/product');
+  const { data, refresh } = useFetch('/api/product');
 
 </script>
--- readme.md
+++ readme.md
@@ -5,27 +5,42 @@
 ## このプロジェクトについて
 * 「ピクミンシリーズチャレンジモード大会（ver.2）」のフルリプレイスプロジェクトです。 2023年夏公開予定です。公開後に旧ピクチャレ大会は閉鎖します。
 
+## 開発に参加したいという方へ
+まずはTwitterかDiscordで管理人へ気軽にお問い合わせください。
+下記の環境構築を試してみていただき、新規ブランチで開発していただく分にはなんの制限もありません。
+devブランチへマージする際にはプルリクエストを投げておいてください。
+
 ## 環境構築手順
 ```shell
+# ソースコードをダウンロード
 $ git clone https://github.com/koppacha/pik5.git
 $ cd pik5
-$ docker compose up -d
-$ cp .env.sample .env
 
-# フロントサーバー用に新しいターミナルを起動
+# 仮想環境コンテナを構築（初回は時間がかかります）
+$ docker compose up -d
+
+# 定数ファイルをコピー
+$ cp sanple.env .env
+
+# 以下実行前にフロントサーバー用に新しいターミナルを起動
 $ docker compose exec nuxt bash
 $ npm install
 $ npm run dev # 終了するときはCtrl+C
 
-# バックエンドサーバー用に新しいターミナルを起動
+# 以下実行前にバックエンドサーバー用に新しいターミナルを起動
 $ docker compose exec laravel bash
+$ composer install
 $ php artisan serve --host 0.0.0.0 # 終了するときはCtrl+C
+
+# 開発を終了する際は以下でコンテナを廃棄する
+$ docker compose down
 ```
+上記実行後、 http://localhost:3000 にアクセス
 
 ## バージョン履歴
 ### ver.1.00〜2.78（2007/04/29〜2023/XX/XX）
 * ver.1：ピクミン３同盟内コンテンツ時代（2007/04/29〜2015/08/31）
-* ver.2：ピクミンシリーズチャレンジモード大会（2015/09/01〜）
+* ver.2：ピクミンシリーズチャレンジモード大会（2015/09/01〜）  
 詳しくは　https://github.com/koppacha/pik4 を参照
 
 ### ver.3.00pre (2022/09/13〜2022/12/31)
