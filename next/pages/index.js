756fdbfa42876c80dd2c1563dd3e64b45def0068
koppacha <pikminchallenge@gmail.com>
2023-06-04 23:39:57 +0900

    トレンド機能の作成

--- next/pages/index.js
+++ next/pages/index.js
@@ -7,27 +7,28 @@ import Navigation from "../components/Layouts/Navigation";
 import {Box, Grid, Typography} from "@mui/material";
 import React from "react";
 import styled from "styled-components";
-import {InfoBox, RuleBox} from "../styles/pik5.css";
+import {CellBox, InfoBox, RuleBox} from "../styles/pik5.css";
 import {useLocale} from "../plugin/pik5";
 import {
     faArrowTrendUp,
     faBullhorn,
     faCertificate,
-    faChartColumn,
+    faChartColumn, faCheckToSlot,
     faFlag,
     faTrophy
 } from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import NewRecords from "../components/NewRecords";
 import PostCountRanking from "../components/PostCountRanking";
+import TrendRanking from "../components/TrendRanking";
 
 const WrapTopBox = styled(Grid)`
   padding :10px;
 `
 const TopBox = styled(Box)`
-  border :1px solid #fff;
-  border-radius :4px;
-  height :100%;
+  border: 1px solid #777;
+  border-radius: 4px;
+  height: 100%;
 `
 const TopBoxHeader = styled(Box)`
   background-color :#fff;
@@ -48,10 +49,28 @@ export default function Home() {
 
     const {t} = useLocale()
 
+    // 総合ランキングへのクイックアクセス
     const totals = [1, 2, 3, 10, 20, 30, 40]
+
+    // クイックアクセス
+    const quickLinks = [
+        ["ログイン", "/login"],
+        ["アカウント作成", "/register"],
+        ["ピクミン1", "/total/10"],
+        ["ピクミン2", "/total/20"],
+        ["ピクミン3", "/total/30"],
+        ["ピクミン4", "/total/40"],
+        ["キーワード", "/keyword"],
+        ["本編RTA", "/speedrun"],
+        ["期間限定", "/limited"],
+        ["全総合", "/total/1"],
+        ["通常総合", "/total/2"],
+        ["特殊総合", "/total/3"],
+    ]
 
   return (
     <>
+        ver.3.00<br/>
         <Typography variant="" className="title">新ピクチャレ大会</Typography><br/>
         <Typography variant="" className="subtitle">Pikmin Series Leaderboards</Typography><br/>
         <InfoBox>
@@ -59,33 +78,27 @@ export default function Home() {
             腕前関係なくどなたでも参加することができます。
         </InfoBox>
         <Grid container>
-            <WrapTopBox item xs={2}>
-                <TopBox>
-                    <TopBoxHeader>
-                        <FontAwesomeIcon icon={faBullhorn} /> クイックアクセス
-                    </TopBoxHeader>
-                    <TopBoxContent>
-                        {
-                            totals.map(n =>
-                                (
-                                    <>
-                                        <QuickLink
-                                            href={"/total/"+n}
-                                        >{t.rule[n]}</QuickLink><br/>
-                                    </>
-                                )
+            <WrapTopBox item xs={12}>
+                <Grid container>
+                    {
+                        quickLinks.map(i =>
+                            (
+                                <Grid item xs={2} component={Link} href={i[1]}>
+                                    <CellBox style={{padding:"10px 0"}}>
+                                        {i[0]}
+                                    </CellBox>
+                                </Grid>
                             )
-                        }
-
-                    </TopBoxContent>
-                </TopBox>
+                        )
+                    }
+                </Grid>
             </WrapTopBox>
-            <Grid item xs={10}>
+            <Grid item xs={12}>
                 <Grid container>
                     <WrapTopBox item xs={6}>
                         <TopBox>
                             <TopBoxHeader>
-                                <FontAwesomeIcon icon={faBullhorn} /> お知らせ
+                                <FontAwesomeIcon icon={faCheckToSlot} /> 日替わり投票
                             </TopBoxHeader>
                             <TopBoxContent>
                                 あ<br/>
@@ -116,10 +129,10 @@ export default function Home() {
                     <WrapTopBox item xs={12}>
                         <TopBox>
                             <TopBoxHeader>
-                                <FontAwesomeIcon icon={faArrowTrendUp} /> 今月のトレンド
+                                <FontAwesomeIcon icon={faArrowTrendUp} /> 最近のトレンド
                             </TopBoxHeader>
                             <TopBoxContent>
-                                あ<br/>
+                                <TrendRanking/>
                             </TopBoxContent>
                         </TopBox>
                     </WrapTopBox>
