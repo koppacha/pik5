cd56d4aae16340118ae8148eb4871a212da26274
koppacha <pikminchallenge@gmail.com>
2023-06-19 22:54:02 +0900

    キーワード画面を目次と本文で分離

---
+++ next/pages/keyword/rules.js
@@ -0,0 +1,13 @@
+export const rules = {
+    unique_id: "rules",
+    flag: 1,
+    category: "terms",
+    keyword: "利用規約・ルール集",
+    tag: "編集保護",
+    yomi: "りようきやく・るーるしゅう",
+    first_editor: "koppacha",
+    last_editor: "koppacha",
+    content: `
+        これはテストです。
+    `
+}
