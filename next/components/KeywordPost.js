a40d4acd5e36d92a967ed0bac1f6f2f9f4f39207
koppacha <pikminchallenge@gmail.com>
2023-06-24 23:50:57 +0900

    キーワード本文のモーダル表示に対応、目次のフィルタリング機能を追加

--- next/components/KeywordPost.js
+++ next/components/KeywordPost.js
@@ -1,11 +1,30 @@
 import {Box, Typography} from "@mui/material";
 import Button from "@mui/material/Button";
 import ReactMarkdown from "react-markdown";
+import Link from "next/link";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faHouseChimney, faStairs} from "@fortawesome/free-solid-svg-icons";
+import {StairIcon} from "../styles/pik5.css";
+import React, {useState} from "react";
+import ModalKeywordEdit from "./ModalKeywordEdit";
+import {mutate} from "swr";
 
-export default function KeywordPost({data, handleEdit}) {
+export default function KeywordPost({data}) {
+
+    const [open, setOpen] = useState(false)
+    const handleOpen = () => {
+        setOpen(true)
+    }
+    const handleClose = () => {
+        setOpen(false)
+        mutate()
+    }
 
     return (
         <>
+            <Link href="/"><FontAwesomeIcon icon={faHouseChimney}/></Link>
+            <StairIcon icon={faStairs}/>
+            <Link href="/keyword">ピクミンキーワード</Link><br/>
             <Button variant="outlined">{data.tag}</Button><br/>
             #{data.unique_id}<br/>
             <Typography variant="" className="mini-title">{data.keyword}</Typography><br/>
@@ -17,17 +36,16 @@ export default function KeywordPost({data, handleEdit}) {
             }}>
                 <ReactMarkdown>
                 {
-                    // キーワード本文は改行コードをbrタグに変換する
                     data.content
                 }
                 </ReactMarkdown>
                 <Button
                     variant="outlined"
-                    value={data.id}
-                    onClick={handleEdit()}
+                    onClick={handleOpen}
                 >
                     編集
                 </Button>
+                <ModalKeywordEdit uniqueId={data.unique_id} open={open} handleClose={handleClose}/>
             </Box>
         </>
     )
--- next/pages/keyword/index.js
+++ next/pages/keyword/index.js
@@ -1,5 +1,5 @@
-import {Box, Typography} from "@mui/material";
-import ModalFormKeyword from "../../components/ModalFormKeyword";
+import {Box, Grid, Typography} from "@mui/material";
+import ModalKeywordEdit from "../../components/ModalKeywordEdit";
 import KeywordPost from "../../components/KeywordPost";
 import Button from "@mui/material/Button";
 import React, {useEffect, useRef, useState} from "react";
@@ -9,121 +9,93 @@ import {InfoBox, StairIcon} from "../../styles/pik5.css";
 import Link from "next/link";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faHouseChimney, faStairs} from "@fortawesome/free-solid-svg-icons";
+import PullDownKeywordCategory from "../../components/PullDownKeywordCategory";
+import {useSearchParams} from "next/navigation";
+import ModalKeyword from "../../components/ModalKeyword";
 
 export default function KeywordIndex(){
 
-    const {data} = useSWR(`http://localhost:8000/api/keyword`, fetcher)
+    const params = useSearchParams()
+    const c = params.get("c")
+    const t = params.get("t")
 
-    // 送信ボタン押下時にデータをポストする
-    const [tag, setTag] = useState("")
-    const [keyword, setKeyword] = useState("")
-    const [yomi, setYomi] = useState("")
-    const [content, setContent] = useState("")
-    const [uniqueId, setUniqueId] = useState("")
-    const [firstEditor, setFirstEditor] = useState("")
+    const p = c ? `?c=${c}` : t ? `?t=${t}` : ""
 
-    // 編集ボタン押下時にデータをリクエストして渡す
-    const [editTag, setEditTag] = useState("")
-    const [editKeyword, setEditKeyword] = useState("")
-    const [editYomi, setEditYomi] = useState("")
-    const [editContent, setEditContent] = useState("")
-    const [editUniqueId, setEditUniqueId] = useState("")
-    const [editFirstEditor, setEditFirstEditor] = useState("")
+    const {data} = useSWR(`http://localhost:8000/api/keyword?${p}`, fetcher)
 
     const [open, setOpen] = useState(false)
+    const [editOpen, setEditOpen] = useState(false)
+    const [uniqueId, setUniqueId] = useState("")
 
-    // 新規キーワードを作成する
-    const handleClickOpen = () => {
-        setEditKeyword("")
-        setEditYomi("")
-        setEditContent("")
-        setKeyword("")
-        setYomi("")
-        setContent("")
-        setUniqueId("")
-        setFirstEditor("")
+    // モーダル制御
+    const handleOpen = (id) => {
+        setUniqueId(id)
         setOpen(true)
     }
-    const handleClose = () => {
+    const handleEditOpen = () => {
         setOpen(false)
-        mutate()
+        setEditOpen(true)
     }
+    const handleClose = () => setOpen(false)
+    const handleEditClose = () => setEditOpen(false)
 
-    // 既存のキーワードを編集する
-    const handleEdit = async (btn) => {
-        const res = await fetch(`http://localhost:8000/api/keyword/${btn.target.value}`)
-        const data = await res.json()
-        setTag(data.tag)
-        setKeyword(data.keyword)
-        setYomi(data.yomi)
-        setContent(data.content)
-        setUniqueId(data.unique_id)
-        setFirstEditor(data.first_editor)
-        setEditTag(data.tag)
-        setEditKeyword(data.keyword)
-        setEditYomi(data.yomi)
-        setEditContent(data.content)
-        setEditUniqueId(data.unique_id)
-        setEditFirstEditor(data.first_editor)
-        setOpen(true)
-    }
+    let hi, mae
 
     return (
         <>
             <Link href="/"><FontAwesomeIcon icon={faHouseChimney}/></Link>
             <StairIcon icon={faStairs}/>
-            <Typography variant="">その他コンテンツ</Typography><br/>
+            <Link href="/keyword">ピクミンキーワード</Link><br/>
             <Typography variant="" className="title">ピクミンキーワード</Typography><br/>
             <Typography variant="" className="subtitle">Pikmin Keywords</Typography><br/>
             <InfoBox>
             ピクミンシリーズ、ピクチャレ大会、ピクミン界隈にまつわる専門用語や流行語などをなんでも保存しておくためのページです。基本的にどなたでも編集できます。
                 <Box style={{ margin: '1em'}}>
                     <ul>
-                        <li>登録できるのはピクミン界隈にある程度受け入れられている言葉に限ります。作成者以外に認知されていない造語等は削除対象です。</li>
-                        <li>キーワード名及び本文は簡潔でわかりやすい表現を心がけてください。</li>
+                        <li>Sorry, this content is Japanese only.</li>
+                        <li>登録できるのはピクミン界隈にある程度受け入れられている概念に限ります。作成者以外に認知されていない概念等は削除対象です。</li>
+                        <li>本文にはMarkdownを使用できます。</li>
+                        <li>キーワード名及び本文はピクミン初心者にもなるべくわかりやすい表現を心がけてください。</li>
                         <li>キーワード名単独でどのシリーズやステージの用語か分かりにくい場合は（）でステージ名等を付け加えてください。その際、よみがなにカッコ及びカッコ内を含める必要はありません。</li>
                         <li>本文中のゲームタイトルは『』で囲んでください。強調したい言葉は「」で囲んでください。（キーワード名に含まれれる場合は不要）</li>
                         <li>本文にプレイヤー名を記述する場合は末尾に「氏」をつけてください。（キーワード名にプレイヤー名が含まれる場合は敬称略としてください）</li>
-                        <li>利用規約やレギュレーションなど一部のキーワードには編集権限が必要になります。</li>
                     </ul>
                 </Box>
             </InfoBox>
-            <Button variant="outlined" onClick={handleClickOpen}>
+            <Button variant="outlined" onClick={handleEditOpen}>
                 キーワードを新規作成
             </Button>
-            <ModalFormKeyword
-                handleClose={handleClose}
-                handleClickOpen={handleClickOpen}
-                editTag={editTag}
-                editKeyword={editKeyword}
-                editYomi={editYomi}
-                editContent={editContent}
-                editUniqueId={editUniqueId}
-                editFirstEditor={editFirstEditor}
-                tag={tag}
-                keyword={keyword}
-                yomi={yomi}
-                content={content}
-                uniqueId={uniqueId}
-                firstEditor={firstEditor}
-                setTag={setTag}
-                setKeyword={setKeyword}
-                setYomi={setYomi}
-                setContent={setContent}
-                setUniqueId={setUniqueId}
-                setFirstEditor={setFirstEditor}
-                open={open}
-                setOpen={setOpen}/>
+            <PullDownKeywordCategory category={c}/>
+            <Grid container>
             {
-                data?.map(post =>
-                    <>
-                        <Link href={"/keyword/"+post.unique_id}>{post.keyword}</Link>
-                        <br/>
-                    </>
-
-                    // <KeywordPost data={post} handleEdit={() => handleEdit}/>
-                )
+                data?.map(function (post){
+                    mae = hi || "わ"
+                    hi = post.yomi
+                    if(hi.slice(0, 1).normalize('NFD')[0] !== mae.slice(0, 1).normalize('NFD')[0]) {
+                        return (
+                            <>
+                                <Grid item xs={12} style={{marginTop:"2em"}}><Typography variant="h3">{hi.slice(0, 1).normalize('NFD')[0]}</Typography></Grid>
+                                <Grid item xs={3} style={{marginBottom:"0.5em",borderBottom:"1px solid #999"}}>
+                                    <Link href={"/keyword?t="+post.tag} style={{color:"#777",fontSize:"0.75em"}}>{post.tag}</Link><br/>
+                                    <Typography style={{cursor:"pointer"}} onClick={()=>handleOpen(post.unique_id)}>{post.keyword}</Typography>
+                                </Grid>
+                            </>
+                        )
+                    } else {
+                        return (
+                            <>
+                                <Grid item xs={3} style={{marginBottom:"0.5em",borderBottom:"1px solid #999"}}>
+                                    <Link href={"/keyword?t="+post.tag} style={{color:"#777",fontSize:"0.75em"}}>{post.tag}</Link><br/>
+                                    <Typography style={{cursor:"pointer"}} onClick={()=>handleOpen(post.unique_id)}>{post.keyword}</Typography>
+                                </Grid>
+                            </>
+                        )
+                    }
+                })
             }
+            </Grid>
+            <ModalKeywordEdit editOpen={editOpen} uniqueId={uniqueId} handleEditClose={handleEditClose}/>
+            <ModalKeyword open={open} uniqueId={uniqueId} handleClose={handleClose} handleEditOpen={handleEditOpen}/>
         </>
     )
 }
