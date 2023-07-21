1a148724e321b3857765f1cd59a92c3f0e545deb
koppacha <pikminchallenge@gmail.com>
2023-07-21 14:50:56 +0900

    軽微修正

--- next/pages/index.js
+++ next/pages/index.js
@@ -87,6 +87,7 @@ export default function Home({users}) {
                                 <FontAwesomeIcon icon={faCheckToSlot} /> ログイン情報
                             </TopBoxHeader>
                             <TopBoxContent>
+                                <Link href="/keyword/moving">旧ピクチャレ大会からの引き継ぎはこちら</Link><br/>
                                 {
                                     (session)
                                         &&
