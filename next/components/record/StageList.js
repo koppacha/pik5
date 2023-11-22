b030d7e75bef3e01cc5ce27b7edd34b35f955c54
koppacha <pikminchallenge@gmail.com>
2023-11-22 16:08:45 +0900

    ver.3.04 URLのパラメータ継承が特定の遷移で効かなくなる不具合を修正

--- next/components/record/StageList.js
+++ next/components/record/StageList.js
@@ -8,18 +8,20 @@ export default function StageList({stages, consoles, rule, year}){
 
     const {t} = useLocale()
 
-
     return (
         <StageListWrapper count={stages.length}>
             <Grid container style={{minWidth:(stages.length > 16) ? "1200px" : "100%"}}
                   columns={{xs:(stages.length > 16) ? 10 : 4, lg : 10}}>
                 {
-                    stages?.map(stage =>
-                        <Grid style={{whiteSpace:"nowrap"}} key={stage} item xs={1}>
-                            <Link key={stage} href={'/stage/'+stage+'/'+consoles+'/'+rule+'/'+year}>
-                                <StageListBox>#{stage}<br/>{t.stage[stage].length > 15 ? t.stage[stage].substring(0, 14)+".." : t.stage[stage]}</StageListBox>
-                            </Link>
-                        </Grid>
+                    stages?.map(function(stage){
+                        const params = (rule) ? `${stage}/${consoles}/${rule}/${year}` : stage
+                        return (
+                            <Grid style={{whiteSpace:"nowrap"}} key={stage} item xs={1}>
+                                <Link key={stage} href={'/stage/'+params}>
+                                    <StageListBox>#{stage}<br/>{t.stage[stage].length > 15 ? t.stage[stage].substring(0, 14)+".." : t.stage[stage]}</StageListBox>
+                                </Link>
+                            </Grid>
+                        )}
                     )
                 }
             </Grid>
