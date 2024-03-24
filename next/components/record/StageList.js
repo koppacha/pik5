cca4a835600763541a79908f208d81365492f53c
koppacha <pikminchallenge@gmail.com>
2024-03-24 17:28:28 +0900

    ver.3.08 軽微修正

--- next/components/record/StageList.js
+++ next/components/record/StageList.js
@@ -6,7 +6,20 @@ import {currentYear, stageUrlOutput, useLocale} from "../../lib/pik5";
 
 export default function StageList({parent, currentStage, stages, consoles, rule, year}){
 
-    const {t} = useLocale()
+    const {t, locale} = useLocale()
+
+    function stageNameFormat(stage){
+        // 文字数制限日本語なら16文字、英語なら32文字（混在は考慮しない）
+        const len = locale === "ja" ? 16 : 32
+        let stageName = t.stage[stage] ?? ""
+        if(stageName.length > len){
+            // 15文字より多かったら14文字にする
+            stageName = stageName.substring(0, len - 1)+".."
+        }
+        // 通常ステージの半角スペースは改行に置き換える
+        stageName = stageName.replace(/([！？１２３４ン:]) /, "$1\n")
+        return stageName ?? ""
+    }
 
     return (
         <StageListWrapper count={stages?.length}>
@@ -17,7 +30,7 @@ export default function StageList({parent, currentStage, stages, consoles, rule,
                         return (
                             <Grid style={{whiteSpace:"nowrap"}} key={stage} item xs={1}>
                                 <Link key={stage} href={'/stage/'+stageUrlOutput(stage, consoles, rule, year, parent)}>
-                                    <StageListBox className={(Number(currentStage) === stage)&&"active"}>#{stage}<br/>{t.stage[stage].length > 15 ? t.stage[stage].substring(0, 14).replace(" ","\n")+".." : t.stage[stage].replace(" ","\n")}</StageListBox>
+                                    <StageListBox className={(Number(currentStage) === stage)&&"active"}>#{stage}<br/>{stageNameFormat(stage)}</StageListBox>
                                 </Link>
                             </Grid>
                         )}
